18e32896d3cf6cce507b278bc0d0f60167b8daba
kerimovok <naxro.ok1@gmail.com>
2024-04-14 21:31:54 +0400

    added<lib>: getAll & onAll methods

--- assets/js/lib/$.js
+++ assets/js/lib/$.js
@@ -3,19 +3,22 @@
  * Inspired by jQuery ☠
  */
 export default class $ {
-    constructor(selector) {
-        this.element = $.get(selector);
-    }
-
-    on(event, callback) {
-        this.element.addEventListener(event, callback);
-    }
-
     static get(selector) {
         return document.querySelector(selector);
     }
 
+    static getAll(selector) {
+        return document.querySelectorAll(selector);
+    }
+
     static on(selector, event, callback) {
         $.get(selector).addEventListener(event, callback);
+    }
+
+    // Could be merged with on() method using a default parameter or with if statement
+    static onAll(selector, event, callback) {
+        $.getAll(selector).forEach((element) =>
+            element.addEventListener(event, callback),
+        );
     }
 }
